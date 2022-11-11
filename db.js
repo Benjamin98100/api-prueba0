dbcf840523f642fc6d805efef72c83e8548c174e
benjasx <benjasanchez175@gmail.com>
2022-11-11 02:57:18 -0700

    first commit

---
+++ db.js
@@ -0,0 +1,10 @@
+const posts = require("./data/posts")
+const comments = require("./data/comments")
+
+const questions = require("./data/questions")
+
+module.exports = {
+    posts,
+    comments,
+    questions
+}
